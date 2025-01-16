7c4fa26539bb1e8167a93ad222f82b5c8bad18a4
kkarim <kkarim@sd61.bc.ca>
2025-01-16 09:53:08 -0800

    Elevator, Claw updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/amainCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/amainCode.java
@@ -16,21 +16,29 @@ public class amainCode extends OpMode {
     private DcMotor frontLeft, frontRight, backRight, backLeft,clipMotor,extentionMotor;
     private Servo rotationServo, clawServo,extentionLeft,extentionRight;
 
-    private double ROTATIONPICKUP = 0.35;
+    private double ROTATIONPICKUP = 0.22;
+    private double ROTATIONPREPICKUP = 0.3;
+    private double ROTATIONNEUTRAL = 0.8;
+    private double ROTATIONTRANSFER = 0.95 ;
+
+
     private double CLAWPICKUP = 0.18;
-   private int  EXTENTIONPICKUP = -128;
+    private double CLAWOPEN = 0;
+    private int  EXTENTIONPICKUP = -128;
     private int  EXTENTIONTRANSFER = 0;
 
-    private double CLAWOPEN = 0;
-    private double ROTATIONTRANSFER = 0 ;
+    private int CLIPMOTORBUCKET = 1000;
+    private int CLIPMOTORPREBUCKET = 900;
+    private int CLIPMOTORBAR = 500;
+    private int CLIPMOTORHOME = 0;
+    private double CLIPMOTORPOWER = 0.5;
+
    // private  double NEUTRALCLAW = 0;
     //private double NEUTRALROTATION = 1;
     private boolean HOMING;
 
-
-
-
-
+    private double extensionPower = 0;
+    private double EXTENSIONPOWERMAX = 0.3;
 
 
     public void init() {
@@ -40,11 +48,10 @@ public class amainCode extends OpMode {
         backLeft = hardwareMap.dcMotor.get("backLeft");
 
         clipMotor = hardwareMap.dcMotor.get("clipMotor");
-
-
-
+        clipMotor.setDirection(DcMotor.Direction.REVERSE);
 
         clawServo = hardwareMap.get(Servo.class,"clawServo");
+        //clawServo.setDirection(Servo.Direction.REVERSE);
         rotationServo = hardwareMap.get(Servo.class,"rotationServo");
         //extentionLeft.setDirection( Servo.Direction.REVERSE);
         //extentionRight.setDirection(Servo.Direction.REVERSE);
@@ -53,9 +60,10 @@ public class amainCode extends OpMode {
         frontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
         clipMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        clipMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         clipMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        clipMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         frontLeft.setDirection(DcMotorEx.Direction.REVERSE);
         frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -82,8 +90,8 @@ public class amainCode extends OpMode {
 
     @Override
     public void loop() {
-        if(HOMING){
-            extentionMotor.setPower(-0.5);
+        if(HOMING){ // homing the extention, so don't do other claw stuff
+            extentionMotor.setPower(-0.2);
             if(limitSwitch.isPressed()){
                 extentionMotor.setPower(0);
                 extentionMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -91,30 +99,72 @@ public class amainCode extends OpMode {
 
                 HOMING = false;
             }
-        }else{
-            extentionMotor.setPower(-gamepad2.left_stick_y);
-            if (gamepad2.dpad_right){
+        }else{  // NOT HOMING extention, so other stuff can happen
+            extensionPower = -gamepad2.left_stick_y;
+            if (extensionPower > EXTENSIONPOWERMAX) { // extension power from stick too big
+                extensionPower = EXTENSIONPOWERMAX;
+            } else if (extensionPower < -EXTENSIONPOWERMAX) { // extension power from stick too big negative
+                extensionPower = -EXTENSIONPOWERMAX;
+            }
+            if ((extensionPower<0) && limitSwitch.isPressed()) { // extension ALL the way in, DON'T move further
+                telemetry.addData("Illegal Retraction!",extensionPower);
+                extentionMotor.setPower(0);
+                // already homed, trying to retract, so no Power!
+            } else {
+                telemetry.addData("Extension Power:", extensionPower);
+                extentionMotor.setPower(extensionPower);
+            }
+
+            // bumpers for sample PICKUP
+            if (gamepad2.right_bumper){
                 clawServo.setPosition(CLAWPICKUP);
-            }else if (gamepad2.dpad_left){
+            } else if (gamepad2.left_bumper) {
                 clawServo.setPosition(CLAWOPEN);
+            }
 
-            }else if (gamepad2.dpad_up){}
+            // dpad for claw ROTATION
+            if (gamepad2.dpad_up) { // pickup
+                rotationServo.setPosition(ROTATIONPICKUP);
+            }
+            else if (gamepad2.dpad_down) {  // pre-pickup
+                rotationServo.setPosition(ROTATIONPREPICKUP);
+            }
+            else if (gamepad2.dpad_left) {  // neutral
+                rotationServo.setPosition(ROTATIONNEUTRAL);
+            }
+            else if (gamepad2.dpad_right) { // transfer
+                rotationServo.setPosition(ROTATIONTRANSFER);
+            }
 
+        } // end of not HOMING for extention
 
-
-
-
-
+        // right hand geometric buttons for clip elevator
+        if (gamepad2.triangle) { // bucket
+            clipMotor.setTargetPosition(CLIPMOTORBUCKET);
+            clipMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            clipMotor.setPower(CLIPMOTORPOWER);
+            telemetry.addData("Elevator going to","bucket");
+        } else if (gamepad2.square) { // prebucket
+            clipMotor.setTargetPosition(CLIPMOTORPREBUCKET);
+            clipMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            clipMotor.setPower(CLIPMOTORPOWER);
+            telemetry.addData("Elevator going to","prebucket");
+        } else if (gamepad2.circle) { // bar
+            clipMotor.setTargetPosition(CLIPMOTORBAR);
+            clipMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            clipMotor.setPower(CLIPMOTORPOWER);
+            telemetry.addData("Elevator going to","bar");
+        } else if (gamepad2.cross) { // home
+            clipMotor.setTargetPosition(CLIPMOTORHOME);
+            clipMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            clipMotor.setPower(CLIPMOTORPOWER);
+            telemetry.addData("Elevator going to","home");
         }
+        int elevatorPosition = clipMotor.getCurrentPosition();
+        telemetry.addData("Elevator Encoder Position:", elevatorPosition);
 
 
-
-
-
-
-
-
-
+        // all the mecanum drive code
         double drive = -gamepad1.left_stick_y;
         double strafe = gamepad1.left_stick_x;
         double turn = gamepad1.right_stick_x;
